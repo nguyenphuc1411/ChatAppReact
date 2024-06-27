f6efd7ab8581222ad0ad36129d2a64e5f3565440
Nguyen Thanh Phuc <nguyenphuc14112003@gmail.com>
2024-06-27 19:39:42 +0700

    install tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ function App() {
 
   return (
     <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </>
   )
 }
